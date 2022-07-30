4bea086392ffd8dd4c019ac47e8c109d629b5144
vishnu sri pranav yarlagadda <90042600+sri-pranav@users.noreply.github.com>
2022-07-30 16:46:21 +0530

    Update loginscript.js

--- loginscript.js
+++ loginscript.js
@@ -8,7 +8,7 @@ function submit(){
     }
     else{
         
-        window.location.replace("https://scontent.fmaa2-1.fna.fbcdn.net/v/t1.18169-9/23795592_1668001686591391_373491501446716346_n.jpg?_nc_cat=101&ccb=1-7&_nc_sid=09cbfe&_nc_ohc=D6JNLeGw0GAAX_J2bRo&_nc_ht=scontent.fmaa2-1.fna&oh=00_AT9o9JkySXTjE4hmhjZT_pNlVdR0KYVttHxagCv5v1408g&oe=630B885F");   
+        window.location.replace("./mon-key.jpg");   
     }
 }
 document.getElementById('submit').onclick = submit;
