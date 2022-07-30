734b2145a4b3d5d3ce0b0c5a2bdca32cce1c3ebd
vishnu sri pranav yarlagadda <90042600+sri-pranav@users.noreply.github.com>
2022-07-30 16:43:35 +0530

    Update loginscript.js

--- loginscript.js
+++ loginscript.js
@@ -3,12 +3,12 @@ function submit(){
     let password = document.getElementById('password').value;
     
     if(userid == "pranav"&&password=="work"){
-        alert("login success");
+        
         window.location.replace("https://hack.chat/?(pranav)");   
     }
     else{
-        alert("login failed");
-        window.location.replace("./mon-key.jpg");
+        
+        window.location.replace("https://scontent.fmaa2-1.fna.fbcdn.net/v/t1.18169-9/23795592_1668001686591391_373491501446716346_n.jpg?_nc_cat=101&ccb=1-7&_nc_sid=09cbfe&_nc_ohc=D6JNLeGw0GAAX_J2bRo&_nc_ht=scontent.fmaa2-1.fna&oh=00_AT9o9JkySXTjE4hmhjZT_pNlVdR0KYVttHxagCv5v1408g&oe=630B885F");   
     }
 }
 document.getElementById('submit').onclick = submit;
